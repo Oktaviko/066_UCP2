7b2eab11f37342c40a06b9e6b96b601b0ee1495f
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2023-11-17 14:43:13 +0700

    P

--- app/src/main/java/com/example/exercise2/HalamanForm.kt
+++ app/src/main/java/com/example/exercise2/HalamanForm.kt
@@ -1,16 +1,41 @@
 package com.example.exercise2
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.exercise2.ui.theme.Exercise2Theme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HalamanForm(
-    onSubmitButtonClick: (MutableList<String>)-> Unit
+    pilihanDosen: List<String>,
+    onSelectionChanged: (String) -> Unit,
+    onSubmitButtonClick: (MutableList<String>)-> Unit,
+    modifier : Modifier = Modifier
 ){
+    var dosenYgDipilih by remember {mutableStateOf("")}
     var namaTxt by rememberSaveable {
         mutableStateOf("")
     }
@@ -22,5 +47,95 @@ fun HalamanForm(
     }
     var judulTxt by remember {
         mutableStateOf("")
+    }
+
+    var listDataTxt : MutableList<String> = mutableListOf(namaTxt, nimTxt, konsenTxt, judulTxt)
+
+    Column(
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize()
+    ){
+        Text(text = "Formulir Pengajuan Skripsi",
+            fontWeight = FontWeight.Bold,
+            fontSize = 30.sp)
+        Spacer(modifier = Modifier.padding(15.dp))
+        OutlinedTextField(value = namaTxt, onValueChange = {namaTxt = it},
+            label = {
+                Text(text = "Nama Mahasiswa")
+            })
+        Spacer(modifier = Modifier.padding(7.dp))
+        OutlinedTextField(value = nimTxt, onValueChange = {nimTxt = it},
+            label = {
+                Text(text = "NIM")
+            })
+        Spacer(modifier = Modifier.padding(7.dp))
+        OutlinedTextField(value = konsenTxt, onValueChange = {konsenTxt = it},
+            label = {
+                Text(text = "Konsentrasi")
+            })
+        Spacer(modifier = Modifier.padding(7.dp))
+        OutlinedTextField(value = judulTxt, onValueChange = {judulTxt = it},
+            label = {
+                Text(text = "Judul Skripsi")
+            })
+        Spacer(modifier = Modifier.padding(20.dp))
+        Column(
+            modifier = Modifier,
+            verticalArrangement = Arrangement.SpaceBetween
+        ){
+            Row(
+                modifier = Modifier.padding(16.dp)
+            ){
+                Column(
+                    modifier = Modifier
+                ){
+                    Text(text = "Dosen Pembimbing 1")
+                    pilihanDosen.forEach { item ->
+                        Row(
+                            modifier = Modifier.selectable(
+                                selected = dosenYgDipilih == item,
+                                onClick = {
+                                    dosenYgDipilih = item
+                                    onSelectionChanged(item)
+                                }
+                            ),
+                            verticalAlignment = Alignment.CenterVertically
+                        ){
+                            RadioButton(selected = dosenYgDipilih == item, onClick = {
+                                dosenYgDipilih = item
+                                onSelectionChanged(item)
+                            })
+                            Text(item)
+                        }
+                    }
+                }
+                Column(
+                    modifier = Modifier
+                ){
+                    Text(text = "Dosen Pembimbing 2")
+                    pilihanDosen.forEach { item ->
+                        Row(
+                            modifier = Modifier.selectable(
+                                selected = dosenYgDipilih == item,
+                                onClick = {
+                                    dosenYgDipilih = item
+                                    onSelectionChanged(item)
+                                }
+                            ),
+                            verticalAlignment = Alignment.CenterVertically
+                        ){
+                            RadioButton(selected = dosenYgDipilih == item, onClick = {
+                                dosenYgDipilih = item
+                                onSelectionChanged(item)
+                            })
+                            Text(item)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/exercise2/OrderViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.exercise2
+
+import androidx.lifecycle.ViewModel
+import com.example.exercise2.data.OrderUIState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class OrderViewModel : ViewModel(){
+    private val _stateUI = MutableStateFlow(OrderUIState())
+    val stateUI : StateFlow<OrderUIState> = _stateUI.asStateFlow()
+
+
+    fun setContact(listData : MutableList<String>){
+        _stateUI.update { stateSaatIni ->
+            stateSaatIni.copy(
+                nama = listData[0],
+                nim = listData[1],
+                konsen = listData[2],
+                judul = listData[3]
+            )
+        }
+    }
+}
